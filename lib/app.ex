592f869ff3f42153c63dcc1858107af79ce1194d
Antoine Reyt <antoine.reyt@kbrwadventure.com>
2017-06-28 13:07:02 +0200

    it works

---
+++ lib/app.ex
@@ -0,0 +1,12 @@
+defmodule HelloRest.App do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      Plug.Adapters.Cowboy.child_spec(:http, Api.Router, [], [port: 4000])
+    ]
+
+    opts = [strategy: :one_for_one, name: :user_sup]
+    Supervisor.start_link(children, opts)
+  end
+end

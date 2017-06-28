d6ee4b8cccf50b66a6468681d79d2af7681fbda6
Antoine Reyt <antoine.reyt@kbrwadventure.com>
2017-06-28 13:44:16 +0200

    fix content-type

--- lib/api/user..ex
+++ lib/api/user..ex
@@ -5,6 +5,7 @@ defmodule Api.User do
   plug :dispatch
 
   get "/:id" do
+    conn = put_resp_content_type(conn, "application/json")
     case KV.get(id) do
       nil   -> send_resp(conn, 404, "")
       value -> send_resp(conn, 200, Poison.encode!(value))

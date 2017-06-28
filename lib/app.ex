3483f715ccf7ed4391d897ce5f2f237ecd443e3c
Antoine Reyt <antoine.reyt@kbrwadventure.com>
2017-06-28 13:18:16 +0200

    add KV to supervision tree

--- lib/app.ex
+++ lib/app.ex
@@ -2,7 +2,10 @@ defmodule HelloRest.App do
   use Application
 
   def start(_type, _args) do
+    import Supervisor.Spec
+
     children = [
+      worker(KV, []),
       Plug.Adapters.Cowboy.child_spec(:http, Api.Router, [], [port: 4000])
     ]
 

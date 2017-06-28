16e430d67aff68fa602d5565e6b1317664f81136
Antoine Reyt <antoine.reyt@kbrwadventure.com>
2017-06-28 14:30:11 +0200

    fix content-type for every request

--- lib/api/user..ex
+++ lib/api/user..ex
@@ -1,11 +1,15 @@
 defmodule Api.User do
   use Plug.Router
 
+  plug :content_type
   plug :match
   plug :dispatch
 
+  def content_type(conn,_opts) do
+    conn |> put_resp_content_type("application/json")
+  end
+
   get "/:id" do
-    conn = put_resp_content_type(conn, "application/json")
     case KV.get(id) do
       nil   -> send_resp(conn, 404, "")
       value -> send_resp(conn, 200, Poison.encode!(value))

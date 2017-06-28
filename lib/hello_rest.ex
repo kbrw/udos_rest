0311a84a4e79bc078ca038d9a7d99736d075636c
Antoine Reyt <antoine.reyt@kbrwadventure.com>
2017-06-28 12:52:08 +0200

    first commit

---
+++ lib/hello_rest.ex
@@ -0,0 +1,18 @@
+defmodule HelloRest do
+  @moduledoc """
+  Documentation for HelloRest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloRest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hello_rest_test.exs
@@ -0,0 +1,8 @@
+defmodule HelloRestTest do
+  use ExUnit.Case
+  doctest HelloRest
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end

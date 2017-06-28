3c66f2d3748d41fa97a21b93f6e5f80c3d93fc83
Antoine Reyt <antoine.reyt@kbrwadventure.com>
2017-06-28 13:58:02 +0200

    add first test

--- test/hello_rest_test.exs
+++ test/hello_rest_test.exs
@@ -1,8 +1,22 @@
 defmodule HelloRestTest do
-  use ExUnit.Case
-  doctest HelloRest
+  use ExUnit.Case, async: false # false, because same store
+  use Plug.Test
+  @user_id "EAFC53"
+  @user_json """
+    {
+      "firstname": "antoine",
+      "lastname": "REYT"
+    }
+  """
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @opts Api.Router.init([])
+
+  test "returns 404 on non existing user" do
+    conn = conn(:get, "/user/1")
+    conn = Api.Router.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 404
+    assert conn.resp_body == ""
   end
 end
